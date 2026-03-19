ec1ba72fea3e9a1ba7577f608b15949cb050aa46
ankita mishra <ankitamishra8763@gmail.com>
2026-03-20 01:10:18 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,80 @@
+// const express = require("express");
+// require("dotenv").config();
+
+// //const { default: mongoose } = require("mongoose");
+// const cors = require("cors");
+// const dbConnect = require("./config/dbConnect");
+// //const User = require("./models/userSchema");
+// const userRoute = require("./routes/userRoutes");
+// const blogRoute = require("./routes/blogRoutes.js");
+// const authRoutes = require("./routes/authRoutes.js");
+// const cloudinaryConfig = require("./config/cloudinaryConfig.js");
+// const app = express();
+
+// // const PORT = process.env.PORT
+// // console.log("PORT" , process.env)
+
+// app.use(express.json()); // when send string data in json form than use this middleware
+// // app.use(cors());
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   }),
+// );
+
+// app.use("/api/v1", userRoute);
+// app.use("/api/v1", blogRoute);
+// app.use("/api/v1", authRoutes);
+
+// app.listen(process.env.PORT || 3000, () => {
+//   console.log("Server Started");
+//   dbConnect();
+//   cloudinaryConfig();
+// });
+
 const express = require("express");
 require("dotenv").config();
-
-//const { default: mongoose } = require("mongoose");
 const cors = require("cors");
+
 const dbConnect = require("./config/dbConnect");
-//const User = require("./models/userSchema");
 const userRoute = require("./routes/userRoutes");
-const blogRoute = require("./routes/blogRoutes.js");
-const authRoutes = require( "./routes/authRoutes.js");
-const cloudinaryConfig = require("./config/cloudinaryConfig.js");
+const blogRoute = require("./routes/blogRoutes");
+const authRoutes = require("./routes/authRoutes");
+const cloudinaryConfig = require("./config/cloudinaryConfig");
+
 const app = express();
 
-// const PORT = process.env.PORT
-// console.log("PORT" , process.env)
+// ✅ Allow both local + deployed frontend
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://ai-blog-platform-fvu2.onrender.com", // 🔥 your frontend deployed URL
+];
 
-app.use(express.json()); // when send string data in json form than use this middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like Postman)
+      if (!origin) return callback(null, true);
 
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("CORS not allowed"));
+      }
+    },
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+
+// ✅ Routes
 app.use("/api/v1", userRoute);
 app.use("/api/v1", blogRoute);
 app.use("/api/v1", authRoutes);
 
+// ✅ Start server
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server Started");
   dbConnect();
