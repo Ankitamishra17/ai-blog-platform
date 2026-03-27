f12ee0a4fc97b8496f3818d31225d4e7ca44232e
ankita mishra <ankitamishra8763@gmail.com>
2026-03-27 15:22:28 +0530

    add firebase

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,9 +1,26 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../serviceAccountKey.json");
+let adminConfig;
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// ✅ If running on production (Render)
+if (process.env.FIREBASE_PRIVATE_KEY) {
+  adminConfig = {
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  };
+}
+// ✅ If running locally
+else {
+  const serviceAccount = require("../serviceAccountKey.json");
+
+  adminConfig = {
+    credential: admin.credential.cert(serviceAccount),
+  };
+}
+
+admin.initializeApp(adminConfig);
 
 module.exports = admin;
